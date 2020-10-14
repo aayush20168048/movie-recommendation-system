1a410fcbc55b3102779c4d952b9c20374ed2e2be
Aayush Chauhan <aayushshivam7@gmail.com>
2020-10-14 19:26:48 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -2,26 +2,29 @@ import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
+TRAIN = 'train'
+TEST = 'test'
+
 #fetch data from model
 data = fetch_movielens(min_rating = 4.0)
 
 #print training and testing data
-print(repr(data['train']))
-print(repr(data['test']))
+print(repr(data[TRAIN]))
+print(repr(data[TEST]))
 
 #create model
 model = LightFM(loss = 'warp')
 
 #train mode
-model.fit(data['train'], epochs=30, num_threads=2)
+model.fit(data[TRAIN], epochs=30, num_threads=2)
 
 #recommender fucntion
 def sample_recommendation(model, data, user_ids):
     #number of users and movies in training data
-    n_users, n_items = data['train'].shape
+    n_users, n_items = data[TRAIN].shape
     for user_id in user_ids:
     	#movies they already like
-        known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
+        known_positives = data['item_labels'][data[TRAIN].tocsr()[user_id].indices]
 
         #movies our model predicts they will like
         scores = model.predict(user_id, np.arange(n_items))
